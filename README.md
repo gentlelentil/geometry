d2d39b69a0fb202a088aa0bbfa0b6f8266b59b0e
gentlelentil <nathaniel-smith@hotmail.co.uk>
2021-07-26 12:19:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Author = Nathaniel Smith
+
+Collection of geometrical functions in Java. Will be extended as time comes.
+These functions are largely converted from the set of functions I wrote in python.
+
---
+++ src/geometry/linalg.java
@@ -0,0 +1,51 @@
+package geometry;
+import java.util.Arrays;
+
+public class linalg {
+
+	public static double[] coordinates (float A, float B, float C) {
+		double[] coords = {A, B, C};
+		return coords;
+	}
+	public static double[] vec_addition (double[] coords_A, double[] coords_B ) {
+		double[] add_AB = new double[coords_A.length];
+		for (int i = 0; i < add_AB.length; i++) {
+			add_AB[i] = coords_A[i] + coords_B[i];
+			
+		}
+		return add_AB;
+			
+		}
+	public static double[] vec_AB (double[] coords_A, double[] coords_B ) {
+		double[] AB = new double[coords_A.length];
+		for (int i = 0; i < AB.length; i++) {
+			AB[i] = coords_A[i] - coords_B[i];
+			
+		}
+		return AB;
+	}
+	
+	
+	public static double dot_product (double[] coords_A, double[] coords_B) {
+		double product = 0.0;
+		for (int i = 0; i < coords_A.length; i++) {
+			double multiply = coords_A[i] * coords_B[i];
+			product += multiply;
+		}
+		return product;
+	}
+	public static double vector_distance (double[] coords_A, double[] coords_B) {
+		double[] AB_vector = vec_AB(coords_A, coords_B);
+		double length = Math.sqrt(dot_product(AB_vector, AB_vector));
+		return length;
+	}
+	
+	public static void main(String[] args) {
+		// Example calculation
+		double[] coordsA = coordinates(1, 1, 1);
+		double[] coordsB = coordinates(2, 2, 2);
+		double length = vector_distance(coordsA, coordsB);
+		System.out.println(length);
+		// returns sqrt 3
+	}
+	}
