6e7297365c6833f6c63ac684eb3a886500614ea8
gentlelentil <nathaniel-smith@hotmail.co.uk>
2021-07-26 15:50:47 +0200

    added normalisaton fct

--- src/geometry/linalg.java
+++ src/geometry/linalg.java
@@ -2,20 +2,36 @@ package geometry;
 import java.util.Arrays;
 
 public class linalg {
-
+	
+	public static double[] normalise (double[] vector) {
+		double vec_norm = 0.0;
+		double[] normalised_vector = new double[vector.length];
+		for (int i = 0; i < vector.length; i++) {
+			double curr_norm = Math.sqrt((Math.pow(vector[i], 2)));
+			vec_norm += curr_norm;
+		if (vec_norm == 0) {
+			return vector;
+		}
+		}
+		for (int j = 0; j < vector.length; j++) {
+			normalised_vector[j] = vector[j] / vec_norm;
+		}
+		return normalised_vector;
+	}
+	
 	public static double[] coordinates (float A, float B, float C) {
 		double[] coords = {A, B, C};
 		return coords;
 	}
+	
 	public static double[] vec_addition (double[] coords_A, double[] coords_B ) {
 		double[] add_AB = new double[coords_A.length];
 		for (int i = 0; i < add_AB.length; i++) {
-			add_AB[i] = coords_A[i] + coords_B[i];
-			
+			add_AB[i] = coords_A[i] + coords_B[i];	
 		}
 		return add_AB;
-			
 		}
+	
 	public static double[] vec_AB (double[] coords_A, double[] coords_B ) {
 		double[] AB = new double[coords_A.length];
 		for (int i = 0; i < AB.length; i++) {
@@ -24,8 +40,7 @@ public class linalg {
 		}
 		return AB;
 	}
-	
-	
+
 	public static double dot_product (double[] coords_A, double[] coords_B) {
 		double product = 0.0;
 		for (int i = 0; i < coords_A.length; i++) {
@@ -33,6 +48,7 @@ public class linalg {
 			product += multiply;
 		}
 		return product;
+		
 	}
 	public static double vector_distance (double[] coords_A, double[] coords_B) {
 		double[] AB_vector = vec_AB(coords_A, coords_B);
